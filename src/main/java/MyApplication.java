60c771e05065669d326c856d13f71e754458ea20
wangshan <wangshan@bizseer.com>
2023-08-09 22:54:42 +0800

    项目初始化

---
+++ src/main/java/MyApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangshan
+ * @date 2023-07-06 22:50
+ */
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class,args);
+    }
+}
